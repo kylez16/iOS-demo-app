fccc50c8f3cb45c2e6917a3fdeb767c4aa6aa9c1
BT_C09_28 <mobileappdev@bths.edu>
2026-04-20 09:13:38 -0400

    Initial Commit

---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
