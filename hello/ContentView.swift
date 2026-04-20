dda59e92636a369551eb5fcc0531e8adbfcba024
BT_C09_28 <mobileappdev@bths.edu>
2026-04-20 09:16:43 -0400

    updated image and text

--- hello/ContentView.swift
+++ hello/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "tv")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, hello!")
         }
         .padding()
     }
