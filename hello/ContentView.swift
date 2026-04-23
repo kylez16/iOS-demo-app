c64724ff28e9ce414fb8bed1fe3035f818681c05
BT_C09_28 <mobileappdev@bths.edu>
2026-04-23 09:19:22 -0400

    changed to HStack

--- hello/ContentView.swift
+++ hello/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        HStack {
             Image(systemName: "tv")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
